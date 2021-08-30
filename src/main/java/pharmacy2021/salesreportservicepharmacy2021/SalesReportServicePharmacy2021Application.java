89c2b12ca85a1d7d78a87a5d7829e8cbf6d15e4b
vizard-xi <kelvin.prince.336@gmail.com>
2021-08-30 02:21:05 +0000

    Used Java Spring Boot Initializer to create a setup for the project

---
+++ src/main/java/pharmacy2021/salesreportservicepharmacy2021/SalesReportServicePharmacy2021Application.java
@@ -0,0 +1,13 @@
+package pharmacy2021.salesreportservicepharmacy2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesReportServicePharmacy2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesReportServicePharmacy2021Application.class, args);
+	}
+
+}
